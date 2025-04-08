8933e96f532b0d0fa70acf438773d0042a1b14de
Isabella-Secchin <bellasecchinvaz@gmail.com>
2025-04-08 10:00:28 -0300

    ....

---
+++ LP3/Atividade listas/ListaComprimisso.java
@@ -0,0 +1,17 @@
+public class ListaComprimisso{
+    private DataV5[] compromissos;
+    private int cap;
+    private int cont;
+
+    public ListaCompromisso(int cap){
+        this.compromissos = new DataV5[cap];
+        this.cap = cap;
+        this.cont = 0;
+    }
+
+    public void adicionarCompromisso(DataV5 data){
+        for(int i = 0; i < cont; i++){
+            
+        }
+    }
+}
